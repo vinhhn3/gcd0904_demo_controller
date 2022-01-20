92c6e78c35bf7f8ad5efcc4e90e066df819790e1
Vinh Hoang <vinhhn3@fe.edu.vn>
2022-01-20 11:31:38 +0700

    createAction

--- src/Controller/TodoController.php
+++ src/Controller/TodoController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Todo;
+use App\Form\TodoType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class TodoController extends AbstractController
@@ -51,4 +54,47 @@ class TodoController extends AbstractController
 
         return $this->redirectToRoute('todo_list');
     }
+
+    /**
+     * @Route("/todo/create", name="todo_create", methods={"GET", "POST"})
+     */
+    public function createAction(Request $request)
+    {
+        $todo = new Todo();
+        $form = $this->createForm(TodoType::class, $todo);
+
+        if ($this->saveChanges($form, $request, $todo)) {
+            $this->addFlash(
+                    'notice',
+                    'Todo addedd ...'
+            );
+            return $this->redirectToRoute('todo_list');
+        }
+
+        return $this->render('todo/create.html.twig', [
+                'form' => $form->createView()
+        ]);
+    }
+
+    public function saveChanges($form, $request, $todo)
+    {
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $todo->setName($request->request->get('todo')['name']);
+            $todo->setCategory($request->request->get('todo')['category']);
+            $todo->setDescription($request->request->get('todo')['description']);
+            $todo->setPriority($request->request->get('todo')['priority']);
+            $todo->setDueDate(\DateTime::createFromFormat('Y-m-d', $request->request->get('todo')['due_date']));
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($todo);
+            $em->flush();
+
+            return true;
+        }
+
+        return false;
+    }
+
+
 }
